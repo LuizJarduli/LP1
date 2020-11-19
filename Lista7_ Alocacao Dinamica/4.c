fa0bd8635237ea827f70bf6b1038db110269d77f
LuizJarduli <50338486+LuizJarduli@users.noreply.github.com>
2020-11-19 15:10:58 -0300

    Update 4.c

--- Lista7_ Alocacao Dinamica/4.c
+++ Lista7_ Alocacao Dinamica/4.c
@@ -1,15 +1,15 @@
 /*
-    4)  Crie uma funÁ„o que receba como par‚metros dois vetores de inteiros: x1 e x2 e as suas
-    respectivas quantidades de elementos: n1 e n2. A funÁ„o dever· retornar um ponteiro para
-    um terceiro vetor, x3, alocado dinamicamente, contendo a intersecÁ„o de x1 e x2 e usar o ponteiro
+    4)  Crie uma fun√ß√£o que receba como par√¢metros dois vetores de inteiros: x1 e x2 e as suas
+    respectivas quantidades de elementos: n1 e n2. A fun√ß√£o dever√° retornar um ponteiro para
+    um terceiro vetor, x3, alocado dinamicamente, contendo a intersec√ß√£o de x1 e x2 e usar o ponteiro
     qtd para retornar o tamanho de x3.
 
     Sendo:
     x1 = {1, 3, 5, 6, 7}
     x2 = {1, 3, 4, 6, 8}
-    x3 ir· conter {1, 3, 6}.
+    x3 ir√° conter {1, 3, 6}.
 
-    Assinatura da funÁ„o:
+    Assinatura da fun√ß√£o:
     int* interseccao(int *x1, int *x2, int n1, int n2, int *qtd);
 */
 #include <stdio.h>
@@ -60,7 +60,7 @@ int main(){
 
     if(x3 == NULL) printf("\nNao existe Interseccao!!!\n");
     else{
-        printf("\Interseccao x3 = ");
+        printf("\nInterseccao x3 = ");
         imprimirConjunto(x3,qtd);
     }
 
