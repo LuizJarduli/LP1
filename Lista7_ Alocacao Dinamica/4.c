46949c205e56607dcc67d2d8fbfaa30639b2b722
LuizJarduli <luizjarduli@gmail.com>
2020-11-19 15:08:11 -0300

    Upload v4

--- Lista7_ Alocacao Dinamica/4.c
+++ Lista7_ Alocacao Dinamica/4.c
@@ -14,41 +14,107 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define N1 5
-#define N2 5
 
 int * interseccao(int *x1, int *x2, int n1, int n2, int *qtd);
 void imprimirConjunto(int *x, int tam);
 void liberarMemoria(int *ptr);
+void Bubble_Sort(int *ptr, int tam);
+void removeRep(int *ptr, int *tam);
 
 int main(){
 
-    int x1[N1] = {1,3,5,6,7}, x2[N2] = {1,3,4,6,8}, *x3, qtd, n1, n2;
+    int *x3, qtd, n1, n2;
 
-    n1 = N1;
-    n2 = N2;
+    printf("Digite o tamanho do primeiro vetor: ");
+    scanf("%d", &n1);
+    printf("Digite o tamanho do segundo vetor: ");
+    scanf("%d", &n2);
+
+    int x1[n1], x2[n2];
+
+    printf("\n");
+    for(int i = 0; i < n1; i++){
+        printf("Digite o %d valor do primeiro vetor: ", i+1);
+        scanf("%d", &x1[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < n2; i++){
+        printf("Digite o %d valor do segundo vetor: ", i+1);
+        scanf("%d", &x2[i]);
+    }
+
     qtd = 0;
 
-    printf("conjunto x1 = ");
+    printf("\nconjunto x1 = ");
     imprimirConjunto(x1,n1);
     printf("conjunto x2 = ");
-    imprimirConjunto(x2,n1);
+    imprimirConjunto(x2,n2);
+
+    removeRep(x1, &n1);
+    removeRep(x2, &n2);
+
+    Bubble_Sort(x1, n1);
+    Bubble_Sort(x2, n2);
 
     x3 = interseccao(x1,x2,n1,n2,&qtd);
 
-    printf("\nInterseccao x3 = ");
-    imprimirConjunto(x3,qtd);
+    if(x3 == NULL) printf("\nNao existe Interseccao!!!\n");
+    else{
+        printf("\Interseccao x3 = ");
+        imprimirConjunto(x3,qtd);
+    }
 
     liberarMemoria(x3);
     return 0;
 }
 
-void imprimirConjunto(int *x, int tam){
-    for(int i = 0; i < tam; i++){
-        if(i == 0) printf("{ %d, ", x[i]);
-        else if(i == tam-1)printf("%d }\n", x[i]);
-        else printf("%d, ", x[i]);
+void removeRep(int *ptr, int *tam){
+    int fila = 0;
+    for(int i = 0; i < *tam-fila; i++){
+        for(int j = i+1; j < *tam-fila; j++){
+            if(*(ptr+j) == *(ptr+i)){
+                for(int x = j; x < *tam-fila;x++){
+                    *(ptr+x) = *(ptr+x+1);
+                }
+                fila++;
+                j--;
+            }
+
+        }
     }
+
+    *tam = *tam - fila;
+    ptr = (int *) realloc(ptr, *tam);
+
+}
+
+void Bubble_Sort(int *ptr, int tam){
+    if(tam > 1){
+        int aux;
+        for(int i = 0; i < tam-1; i++){
+            for(int j = i+1; j < tam; j++){
+                if(*(ptr+i) > *(ptr+j)){
+                    aux = *(ptr+i);
+                    *(ptr+i) = *(ptr+j);
+                    *(ptr+j) = aux;
+                }
+            }
+        }
+    }
+
+}
+
+void imprimirConjunto(int *x, int tam){
+    if(tam > 1){
+        for(int i = 0; i < tam; i++){
+            if(i == 0) printf("{ %d, ", x[i]);
+            else if(i == tam-1)printf("%d }\n", x[i]);
+            else printf("%d, ", x[i]);
+        }
+    } else {
+        printf("{ %d }\n", x[0]);
+    }
+
 
 }
 
@@ -83,4 +149,3 @@ int * interseccao(int *x1, int *x2, int n1, int n2, int *qtd){
     ptr = (int *) realloc(ptr, (*qtd) * sizeof(int));
     return ptr;
 }
-
